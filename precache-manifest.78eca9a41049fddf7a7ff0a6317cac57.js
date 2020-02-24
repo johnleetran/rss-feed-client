e16f0e3e8401f97526ed858e0d1145153538cf04
John Tran <johnleetran@yahoo.com>
2020-02-23 21:09:15 -0800

    Updates

---
+++ precache-manifest.78eca9a41049fddf7a7ff0a6317cac57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad899a72988168366b4985c3ec180bdb",
+    "url": "/rss-feed-client/index.html"
+  },
+  {
+    "revision": "fd9cfed3c275f1a5c6c9",
+    "url": "/rss-feed-client/static/css/main.6d121e43.chunk.css"
+  },
+  {
+    "revision": "cb9db8caaf807ba43f5b",
+    "url": "/rss-feed-client/static/js/2.3b4fa183.chunk.js"
+  },
+  {
+    "revision": "111410abe44da3bf0d9979ab3d44c08d",
+    "url": "/rss-feed-client/static/js/2.3b4fa183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9cfed3c275f1a5c6c9",
+    "url": "/rss-feed-client/static/js/main.a3534d1d.chunk.js"
+  },
+  {
+    "revision": "54dec29e8577abe5e97d",
+    "url": "/rss-feed-client/static/js/runtime-main.d1a81bea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rss-feed-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rss-feed-client/precache-manifest.78eca9a41049fddf7a7ff0a6317cac57.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rss-feed-client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
